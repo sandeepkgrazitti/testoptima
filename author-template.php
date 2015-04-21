8889c1527fe429a8d6039eb0bacc2cba7249eea5
sachinjgrazitti <sachinj@grazitti.com>
2015-04-21 15:19:23 +0530

    dvsdsdfds

--- author-template.php
+++ author-template.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Author Template functions for use in themes.
- *
+ * sdnkfndkn
  * These functions must be used within the WordPress Loop.
  *
  * @link http://codex.wordpress.org/Author_Templates
